47b406d0d0f10dc06539c681e41a34f3f5c69f89
Darehshori.A <acc.mezone@gmail.com>
2025-09-21 13:36:31 +0330

    None :(

---
+++ Texitor.Control/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Texitor.Control.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+
+    public static IServiceCollection AddTexitor(this IServiceCollection services)
+    {
+
+        return services;
+    }
+
+}
